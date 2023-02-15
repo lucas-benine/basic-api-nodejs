be45e9deab528aae85ca08bd0b95b527a24583f8
lucas_benine <lucasbenine41@gmail.com>
2023-02-14 23:27:25 -0300

    Requisição POST de cadastro de produto implementada

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { randomUUID } = require('crypto');
 
 const app = express();
 
-app.get('/primeira-rota', (request, response) => {
-    return response.json({
-        message: 'Minha primeira rota'
-    })
+app.use(express.json());
+
+const products = [];
+
+app.post('/products', (request, response) => {
+    const { name, price } = request.body;
+    
+    const product = {
+        name: name,
+        price: price,
+        id: randomUUID(),
+    }
+
+    products.push(product);
+    return response.json(product);
 })
 
 app.listen(4002, () => console.log('O servidor está rodando na porta 4002'));
