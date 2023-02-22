07b0e173e97f5cd3b030611f168f9094aa02e16d
lucas_benine <lucasbenine41@gmail.com>
2023-02-21 23:22:23 -0300

    adicionando metodos para atualizar e deletar o produto

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use(express.json());
 const products = [];
 
 app.post('/insertProduct', (request, response) => {
-    const { name, price } = request.body;
-    
+    const { name, price } = request.body;    
     const product = {
         name: name,
         price: price,
@@ -26,8 +25,30 @@ app.get('/findAllProducts', (request, response) => {
 
 app.get('/findProduct/:id', (request, response) => {
     const { id } = request.params;
-    const product = products.find(p => p.id == (id));
+    const product = products.find(p => p.id === (id));
+    
     return response.json(product);
-})
+});
+
+app.put('/updateProduct/:id', (request, response) => {
+    const { id } = request.params;
+    const { name, price } = request.body;
+    const productIndex = products.findIndex((p) => p.id === (id));
+    
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price
+    }
+    return response.json({message: 'Produto alterado com sucesso!'});
+});
+
+app.delete('/deleteProduct/:id', (request, response) => {
+    const { id } = request.params;
+    const productIndex = products.findIndex((p) => p.id === (id));
+
+    products.splice(productIndex, 1);
+    return response.json({message: 'Produto deletado com sucesso!'});
+});
 
 app.listen(4002, () => console.log('O servidor está rodando na porta 4002'));
